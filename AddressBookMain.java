afa8f1da302953af4b9b0e50bd50d85c7063017f
sushant <sgharge299@gmail.com>
2022-07-04 15:55:27 +0530

    [sushant]Implimated the method to adding new contact

--- AddressBookMain.java
+++
@@ -1,7 +0,0 @@
-package AddressBook;
-
-public class AddressBookMain {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
-    }
-}
--- services/AddressBookMain.java
+++ services/AddressBookMain.java
@@ -13,11 +13,48 @@ public class AddressBookMain {
     //This is the main method, Here is the starting point of the program.
 
     public static void main(String[] args) {
-        addPersonDetail();
+
+        boolean isExit = false;
+
+        do {
+            System.out.println("\n\t\tAddress Book System");
+            System.out.println("\n\t\tEnter A to Add Person ");
+            System.out.println("\t\tEnter E to Edit Person");
+            System.out.println("\t\tEnter D to Delete Person");
+            System.out.println("\t\tEnter S to Show Person Detail");
+            System.out.println("\t\tEnter Q to Quit ");
+            System.out.print("\t\tPlease Select One Option : ");
+            char userInput = scanner.nextLine().toUpperCase().charAt(0);
+            switch (userInput) {
+                case 'A':
+                    //add
+                    addPersonDetail();
+                    break;
+                case 'E':
+                    //edit
+                    break;
+                case 'D':
+                    //delete
+                    break;
+                case 'S':
+                    //Show
+                    System.out.println("\n\t\t" + personMap.get(personMap.size() - 1).toString());
+                    break;
+                case 'Q':
+                    //quit
+                    isExit = true;
+                    break;
+                default:
+                    System.out.println("Please select correct option");
+            }
+        } while (!isExit);
+
+
     }
 
     //Method for taking person details and store them into storage.
     //In this program used hashmap.
+
     private static void addPersonDetail() {
         Person person = new Person();
         System.out.print("Enter First Name : ");
@@ -28,8 +65,6 @@ public class AddressBookMain {
         person.setAddress(scanner.nextLine());
         System.out.print("Enter City : ");
         person.setCity(scanner.nextLine());
-        System.out.print("Enter State : ");
-        person.setState(scanner.nextLine());
         System.out.print("Enter ZipCode : ");
         person.setZip(scanner.nextLine());
         System.out.print("Enter Phone Number : ");
@@ -39,4 +74,3 @@ public class AddressBookMain {
         System.out.println(personMap.toString());
     }
 }
-
