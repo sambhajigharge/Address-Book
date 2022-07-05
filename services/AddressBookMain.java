4818f7548c7c80298968e872191de31a00886df4
sushant <sgharge299@gmail.com>
2022-07-05 12:32:29 +0530

    [sushant]worked on program which is adding the multiple address books

--- services/AddressBookMain.java
+++ services/AddressBookMain.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class AddressBookMain {
     private static Scanner scanner = new Scanner(System.in);
     private static Map<String, Person> personMap = new HashMap();
+    private static Map<String, Map<String, Person>> addressBookMap = new HashMap();
 
     //This is the main method, Here is the starting point of the program.
 
@@ -18,7 +19,7 @@ public class AddressBookMain {
 
         do {
             System.out.println("\n\t\tAddress Book System");
-            System.out.println("\n\t\tEnter A to Add Person ");
+            System.out.println("\n\t\tEnter A to Add Book ");
             System.out.println("\t\tEnter E to Edit Person");
             System.out.println("\t\tEnter D to Delete Person");
             System.out.println("\t\tEnter S to Show Person Detail");
@@ -28,7 +29,7 @@ public class AddressBookMain {
             switch (userInput) {
                 case 'A':
                     //add
-                    addPersonDetail();
+                    addBook();
                     break;
                 case 'E':
                     //edit
@@ -54,34 +55,24 @@ public class AddressBookMain {
 
     }
 
-    //Method for taking person details and store them into storage.
-    //In this program used hashmap. Using that multiple person can add.
-
-    private static void addPersonDetail() {
-        Person person = new Person();
-        person = contactFields();
-        personMap.put(person.getFirstName(), person);
-        System.out.println(personMap.toString());
-    }
-
     //Method For editing the existing person details on the basis of first name.
 
     private static void editContact() {
         System.out.print("\nEnter the first name of the person to edit : ");
         String firstName = scanner.nextLine();
-        Person newPerson = personMap.get(firstName);
-        System.out.println(newPerson.toString());
-        if (personMap.get(firstName) != null) {
-            Person person = contactFields();
-            for (int j = 0; j < personMap.size(); j++) {
-                if (personMap.get(firstName).getFirstName().equalsIgnoreCase(newPerson.getFirstName())) {
-                    personMap.put(person.getFirstName(), person);
-                }
-            }
+        System.out.print("\nEnter the city name of the person to edit : ");
+        String cityName = scanner.nextLine();
+        personMap = addressBookMap.get(cityName);
+        System.out.println(personMap.toString());
+
+        if (addressBookMap.get(cityName).get(firstName) != null) {
+            Person editedPerson = contactFields();
+            personMap.put(editedPerson.getFirstName(), editedPerson);
+            addressBookMap.put(editedPerson.getCity(), personMap);
         } else {
-            System.out.println("Record not found");
+            System.out.println("Record Not Found");
         }
-        System.out.println("\n\t\t" + personMap.toString());
+        System.out.println("\n\t\t" + addressBookMap.toString());
     }
 
     //Method for taking person details in Person-Model format.
@@ -104,17 +95,32 @@ public class AddressBookMain {
     }
 
     //Method for deleting the person from existing address book
-    public static void deletePerson() {
+    private static void deletePerson() {
         System.out.print("\nEnter the first name of the person to delete : ");
         String firstName = scanner.nextLine();
         Person newPerson = personMap.get(firstName);
         System.out.println(newPerson.toString());
-        if (newPerson != null) {
+        if (personMap.get(firstName) != null) {
             personMap.remove(firstName);
             System.out.println("Deleted Successfully");
         } else {
             System.out.println("Record not exist");
         }
+    }
 
+    //Method for taking person details and store them into storage with
+    //their city name reference.
+    //In this program used hashmap. Multiple person can add.
+    private static void addBook() {
+        Map<String, Person> newPersonMap = new HashMap();
+        Person newPerson;
+        newPerson = contactFields();
+
+        if (addressBookMap.get(newPerson.getCity()) != null) newPersonMap = addressBookMap.get(newPerson.getCity());
+
+        newPersonMap.put(newPerson.getFirstName(), newPerson);
+        addressBookMap.put(newPerson.getCity(), newPersonMap);
+
+        System.out.println("\n\t\t" + addressBookMap.toString());
     }
 }
